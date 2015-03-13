e4f1f7c73b62ea8bbba8294d8a5f5db5eaaeaba4
Connor Abdelnoor <connor.abd@gmail.com>
2015-03-13 10:05:49 -0700

    Write the last 3 tests writtin in initial spec outline, all of which pass with method unchanged.

--- tests/CounterTest.php
+++ tests/CounterTest.php
@@ -65,6 +65,51 @@
 
         }
 
+        function test_check_singleWordTwice()
+        {
+            //Arrange
+            $test_Counter = new RepeatCounter;
+            $input_string = "big big house";
+            $input_search = "big";
+
+            //Act
+            $result = $test_Counter->countRepeats($input_string, $input_search);
+
+            //Assert
+            $this->assertEquals(2, $result);
+
+        }
+
+        function test_check_singleLetterWordInString()
+        {
+            //Arrange
+            $test_Counter = new RepeatCounter;
+            $input_string = "there was a large mouse";
+            $input_search = "a";
+
+            //Act
+            $result = $test_Counter->countRepeats($input_string, $input_search);
+
+            //Assert
+            $this->assertEquals(1, $result);
+
+        }
+
+        function test_check_singleLetter_noResults()
+        {
+            //Arrange
+            $test_Counter = new RepeatCounter;
+            $input_string = "hello world";
+            $input_search = "o";
+
+            //Act
+            $result = $test_Counter->countRepeats($input_string, $input_search);
+
+            //Assert
+            $this->assertEquals(0, $result);
+
+        }
+
 
 
 
